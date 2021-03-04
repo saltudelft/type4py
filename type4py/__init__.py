216c0cc90526e122754618eab46000b60c1c630f
mir-am <mir-am@hotmail.com>
2021-03-04 10:18:34 +0100

    Add logger for the package

--- type4py/__init__.py
+++ type4py/__init__.py
@@ -1,1 +1,13 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logger_sh = logging.StreamHandler()
+logger_sh.setLevel(logging.DEBUG)
+logger.addHandler(logger_sh)
+
+logger_formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(message)s')
+logger_sh.setFormatter(logger_formatter)
+
 __version__ = "0.1"
