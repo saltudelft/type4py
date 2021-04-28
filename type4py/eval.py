e0b8493781b166cb7f1cb5e9e607a512fb14ea0a
Amir Mir <mir-am@hotmail.com>
2021-04-28 17:01:28 +0200

    For eval, considering MRR instead of weighted metrics and also different Type4Py configs

--- type4py/eval.py
+++ type4py/eval.py
@@ -1,7 +1,7 @@
 from type4py import logger
 from libsa4py.utils import load_json
 from os.path import join
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, precision_score
 from tqdm import tqdm
 from typing import List, Tuple
 import pickle
@@ -105,25 +105,21 @@ def eval_pred_dsl(test_pred: List[dict], common_types, tasks: set, top_n=10):
     param_type_match = r'(.+)\[(.+)\]'
 
     def pred_types_fix(y_true: str, y_pred: List[Tuple[str, int]]):
-        for p, _ in y_pred[:top_n]:
+        for i, (p, _) in enumerate(y_pred[:top_n]):
             if p == y_true:
-                return p
+                return p, 1/(i+1)
 
-        return y_pred[0][0]
-
-    # def is_param_correct(y: str, p: str) -> int:
-    #     param_type_match = r'(.+)\[(.+)\]'
-    #     if re.match(param_type_match, p):
-    #         if re.match(param_type_match, y).group(1) == re.match(param_type_match, p).group(1):
-    #             return 1
-  
-    #     return 0
+        return y_pred[0][0], 0.0
 
     def is_param_correct(true_param_type: str, pred_types: np.array):
         no_match = 0
         for p in pred_types:
             if re.match(param_type_match, p):
                 if re.match(param_type_match, true_param_type).group(1) == re.match(param_type_match, p).group(1):
+                    no_match += 1
+                    break
+            else:
+                if re.match(param_type_match, true_param_type).group(1).lower() == p.lower():
                     no_match += 1
                     break
         
@@ -144,18 +140,16 @@ def eval_pred_dsl(test_pred: List[dict], common_types, tasks: set, top_n=10):
     all_param_rare_types = 0
     corr_param_rare_types = 0
 
-    y_true = []
-    y_pred = []
-
+    mrr = []
+   
     for p in tqdm(test_pred, total=len(test_pred)):
 
         if p['task'] not in tasks:
             continue
 
-        top_n_pred = pred_types_fix(p['original_type'], p['predictions'])
-        y_true.append(p['original_type'])
-        y_pred.append(top_n_pred)
-
+        top_n_pred, r = pred_types_fix(p['original_type'], p['predictions'])
+        mrr.append(r)
+        
         if p['original_type'] in ubiquitous_types:
             all_ubiq_types += 1
             if p['original_type'] == top_n_pred:
@@ -183,50 +177,20 @@ def eval_pred_dsl(test_pred: List[dict], common_types, tasks: set, top_n=10):
     logger.info(f"Type4Py - {tasks} - Parametric match - common: {(corr_param_common_types + corr_common_types) / all_common_types * 100.0:.2f}%")
     logger.info(f"Type4Py - {tasks} - Parametric match - rare: {(corr_param_rare_types+corr_rare_types) / all_rare_types * 100.0:.2f}%")
     
-    # res = classification_report(y_true, y_pred, output_dict=True)
+    logger.info(f"Type4Py - Mean reciprocal rank {np.mean(mrr)*100:.2f}")
+   
+    return np.mean(mrr)*100
 
-    # logger.info("F1-score: %.2f | Recall: %.2f | Precision: %.2f" % (res['f1-score']*100,
-    #                                                            res['recall']*100,
-    #                                                            res['precision']*100))
+def evaluate(output_path: str, data_name: str, tasks: str, top_n: int=10):
 
-def evaluate(output_path: str, data_loading_funcs: dict, tasks: str, top_n: int=10):
-
-    logger.info(f"Evaluating Type4Py model for {data_loading_funcs['name']} prediction task")
+    logger.info(f"Evaluating the Type4Py {data_name} model for {tasks} prediction task")
     logger.info(f"*************************************************************************")
     # Loading label encoder andd common types
-    test_pred = load_json(join(output_path, 'type4py_test_predictions.json'))
+    test_pred = load_json(join(output_path, f'type4py_{data_name}_test_predictions.json'))
     le_all = pickle.load(open(join(output_path, "label_encoder_all.pkl"), 'rb'))
     common_types = pickle.load(open(join(output_path, "combined_common_types.pkl"), 'rb'))
     common_types = set(le_all.inverse_transform(list(common_types)))
     ubiquitous_types = {'str', 'int', 'list', 'bool', 'float'}
-    #ubiquitous_types = set(le_all.transform(list(ubiquitous_types)))
     common_types = common_types - ubiquitous_types
-
-
-    # pred_test_embed = np.load(join(output_path, f"type4py_{data_loading_funcs['name']}_pred.npy"), allow_pickle=True)
-    # embed_test_labels = np.load(join(output_path, f"type4py_{data_loading_funcs['name']}_true.npy"))
-
-    # acc_all, acc_ubiq, acc_common, acc_rare, com_mask, rare_mask = eval_type_embed(pred_test_embed,
-    #                                                                  embed_test_labels,
-    #                                                                  ubiquitous_types,
-    #                                                                  common_types, top_n)
-
-    # logger.info("Type4Py - Exact match - all: %.2f%%" % acc_all)
-    # logger.info("Type4Py - Exact match - ubiquitous: %.2f%%" % acc_ubiq)
-    # logger.info("Type4Py - Exact match - common: %.2f%%" % acc_common)
-    # logger.info("Type4Py - Exact match - rare: %.2f%%" % acc_rare)
-
-    # acc_all_param, acc_common_param, acc_rare_param = eval_parametric_match(pred_test_embed,
-    #                                                                         embed_test_labels,
-    #                                                                         ubiquitous_types,
-    #                                                                         common_types, le_all, top_n)
-
-    # logger.info("Type4Py - Parametric match - all: %.2f%%" % acc_all_param)
-    # logger.info("Type4Py - Parametric match - common: %.2f%%" % acc_common_param)
-    # logger.info("Type4Py - Parametric match - rare: %.2f%%" % acc_rare_param)
-
+    
     eval_pred_dsl(test_pred, common_types, tasks, top_n=top_n)
-
-    # logger.info("F1-score: %.2f | Recall: %.2f | Precision: %.2f" % (res['f1-score']*100,
-    #                                                            res['recall']*100,
-    #                                                            res['precision']*100))
