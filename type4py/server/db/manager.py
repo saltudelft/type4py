151462ca5782a213fd1fb93bc68ba4e83449094b
Amir Mir <mir-am@hotmail.com>
2021-07-11 16:56:26 +0200

    Store users' hashed IP address to respect their privacy

--- type4py/server/db/manager.py
+++ type4py/server/db/manager.py
@@ -11,22 +11,16 @@ class PredictReqs(sqla.Model):
     Represents the table predict_reqs which stores requests to the server plus
     their timestamps and errors if any
     """
-
-    # __table__ = sqla.Model.metadata['predict_reqs']
-
-    # def __repr__(self):
-    #     return self.DISTRICT
-    
     __tablename__ = 'predict_reqs'
 
     id = sqla.Column(sqla.Integer, primary_key=True)
-    IP_addr = sqla.Column(sqla.String, nullable=False)
+    hashed_IP = sqla.Column(sqla.String, nullable=False)
     start_t = sqla.Column(sqla.DateTime, nullable=False)
     finished_t = sqla.Column(sqla.DateTime, nullable=False)
     error = sqla.Column(sqla.String)
 
-    def __init__(self, IP_addr, start_t, finished_t, error):
-        self.IP_addr = IP_addr
+    def __init__(self, hashed_IP, start_t, finished_t, error):
+        self.hashed_IP = hashed_IP
         self.start_t= start_t
         self.finished_t = finished_t
         self.error = error
--- type4py/server/response.py
+++ type4py/server/response.py
@@ -2,6 +2,7 @@ from type4py.server.db import manager as dbm
 from flask import session
 from flask_limiter.util import get_remote_address
 from datetime import datetime
+from hashlib import sha1
 
 class ServerResponse:
     def __init__(self, response: dict, error: str=None) -> None:
@@ -9,6 +10,7 @@ class ServerResponse:
         self.error = error
     
     def get(self):
-        dbm.sqla.session.add(dbm.PredictReqs(get_remote_address(), session.get("req_start_t"), datetime.now(), self.error))
+        dbm.sqla.session.add(dbm.PredictReqs(sha1(get_remote_address().encode()).hexdigest(), session.get("req_start_t"),
+                                             datetime.now(), self.error))
         dbm.sqla.session.commit()
         return {'response': self.response, 'error': self.error}
