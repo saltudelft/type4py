37628646015976103ed95c5d2f2c6aec8b8f44dc
Amir Mir <mir-am@hotmail.com>
2021-08-31 13:44:47 +0200

    Add a predict endpoint to support CORS and for the VSCode Web

--- type4py/server/api.py
+++ type4py/server/api.py
@@ -1,9 +1,10 @@
-from flask import render_template, request, Blueprint, session
+from flask import render_template, request, Blueprint, session, jsonify
 from type4py.server.app import app
 from type4py.server.response import PredictResponse, AcceptTypeResponse, is_session_id_valid
 from type4py.infer import PretrainedType4Py, type_annotate_file, get_type_checked_preds
 from datetime import datetime
 from secrets import token_urlsafe
+import json
 
 bp = Blueprint('type4py_api', __name__, template_folder='templates', url_prefix="/api/")
 
@@ -20,8 +21,7 @@ def load_type4py_model():
 @app.before_request
 def before_request_f():
     session['req_start_t'] = datetime.now()
-
-    if request.endpoint == "type4py_api.predict":
+    if request.endpoint == "type4py_api.predict" or request.endpoint == "type4py_api.predict_fetch":
         session['session_id'] = token_urlsafe(32)
         app.logger.info(f"Endpoint {request.endpoint}")
 
@@ -29,26 +29,45 @@ def before_request_f():
 def hello_world():
     return render_template('index.html')
 
-@bp.route('/predict', methods = ['POST', 'GET'])
-def predict():
-    """
-    POST method for uploading a file. Reads in a sent file and returns it.
-    TODO: modify to your own needs
-    """
-    src_file = request.data
-    is_fp_enabled = bool(int(request.args.get("fp"))) if request.args.get("fp") is not None else True
-    session['file_hash'] = request.args.get('fh')
-    session['ext_ver'] = request.args.get('ev')
-    session['act_id'] = request.args.get('ai')
+def predict_type4py_model(src_file: str, **args) -> dict:
+
+    is_fp_enabled = bool(int(args.get("fp"))) if args.get("fp") is not None else True
+    session['file_hash'] = args.get('fh')
+    session['ext_ver'] = args.get('ev')
+    session['act_id'] = args.get('ai')
 
     # if len(request.data.splitlines()) > app.config['MAX_LOC']:
     #     return PredictResponse(None, f"File is larger than {app.config['MAX_LOC']} LoC").get()
     
-    if bool(int(request.args.get("tc"))):
+    if bool(int(args.get("tc"))):
         return PredictResponse(None, "Type-checking is not available yet!").get()
         #return ServerResponse(get_type_checked_preds(type_annotate_file(t4py_pretrained_m, src_file, None), src_file)).get()
     else:
         return PredictResponse(type_annotate_file(t4py_pretrained_m, src_file, None, is_fp_enabled)).get()
+
+
+@bp.route('/predict', methods = ['POST', 'GET'])
+def predict():
+    """
+    Queries the Type4Py model and returns the predicted type annotations as JSON.
+    """
+    return predict_type4py_model(request.data, tc=request.args.get("tc"), fp=request.args.get("fp"), fh=request.args.get('fh'),
+                                 ev=request.args.get('ev'), ai=request.args.get('ai'))
+
+@bp.route('/predict/fetch', methods = ['POST'])
+def predict_fetch():
+    """
+    The predict endpoint to be used for the Fetch API
+    """
+
+    args = json.loads(request.data.decode('utf-8'))
+
+    r = jsonify(predict_type4py_model(args['f'], **args))
+    r.headers.add('Access-Control-Allow-Origin', '*')
+    r.headers.add('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS')
+    r.headers.add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    return r
+
 
 @bp.route('/telemetry/accept_type', methods = ['GET'])
 def submit_accepted_types():
